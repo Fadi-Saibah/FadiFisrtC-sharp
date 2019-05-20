7220eed498f156cb74ecc5d3bf921e72d28c23f6
Fadi-Saibah <50773796+Fadi-Saibah@users.noreply.github.com>
2019-05-20 05:18:36 +0300

    Fadi

--- FisrtApp/FisrtApp/Program.cs
+++ FisrtApp/FisrtApp/Program.cs
@@ -10,9 +10,72 @@ namespace FisrtApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fadi");
-            Console.WriteLine("Fadi");
-            Console.ReadLine();
+
+            //byte  + عدد صحيح
+            //    sbyte +-
+            //    short +-
+            //    ushort +
+            //    int +-
+            //    uint +
+            //    long +-
+            //    ulong +
+            //    float عشرية +-
+            //    double عشرية +-
+            //    decimal عشرية +-
+            //    char حرف واحد
+            //    string
+            //    bool    T.F
+            //    object لأي شي
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //System.Console.WriteLine("Fadi");
+            //Console.WriteLine("ali");
+
+            int num1;
+            num1 = 3;
+
+            string Name;
+            Name = "Hi";
+
+            int Fadi;
+            Fadi = 4;
+
+            char F;
+            F = 'Y';
+
+            bool t = true;
+
+            Double number = 25.3;
+
+            
+            object R = 12;
+
+            bool f = false;
+
+
+            Console.WriteLine(num1);
+            Console.WriteLine(Name);
+            Console.WriteLine(Fadi);
+            Console.WriteLine(F);
+            Console.WriteLine(t);
+            Console.WriteLine(number);
+            Console.WriteLine(R);
+            Console.WriteLine(f);
+            
             Console.Read();
         }
     }
