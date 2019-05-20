7806ccc974cdd3782c28c28ece35c805e257671f
Fadi-Saibah <50773796+Fadi-Saibah@users.noreply.github.com>
2019-05-20 08:19:26 +0300

    Fadi

--- FisrtApp/FisrtApp/Program.cs
+++ FisrtApp/FisrtApp/Program.cs
@@ -11,12 +11,12 @@ namespace FisrtApp
         static void Main(string[] args)
         {
 
-            //byte  + عدد صحيح
+            //byte  + عدد صحيح     255
             //    sbyte +-
-            //    short +-
-            //    ushort +
+            //    short +-  
+            //    ushort + 65535
             //    int +-
-            //    uint +
+            //    uint +  4 millon
             //    long +-
             //    ulong +
             //    float عشرية +-
@@ -25,17 +25,7 @@ namespace FisrtApp
             //    char حرف واحد
             //    string
             //    bool    T.F
-            //    object لأي شي
-
-
-
-
-
-
-
-
-
-
+            //    object 
 
 
 
@@ -45,36 +35,111 @@ namespace FisrtApp
             //System.Console.WriteLine("Fadi");
             //Console.WriteLine("ali");
 
-            int num1;
-            num1 = 3;
+            //int num1;
+            //num1 = 3;
 
-            string Name;
-            Name = "Hi";
+            //string Name;
+            //Name = "Hi";
 
-            int Fadi;
-            Fadi = 4;
+            //int Fadi;
+            //Fadi = 4;
 
-            char F;
-            F = 'Y';
+            //char F;
+            //F = 'Y';
 
-            bool t = true;
+            //bool t = true;
 
-            Double number = 25.3;
+            //Double number = 25.3;
+
+
+            //object R = 12;
+
+            //bool f = false;
+
+
+            //Console.WriteLine(num1);
+            //Console.WriteLine(Name);
+            //Console.WriteLine(Fadi);
+            //Console.WriteLine(F);
+            //Console.WriteLine(t);
+            //Console.WriteLine(number);
+            //Console.WriteLine(R);
+            //Console.WriteLine(f);
+
+
+            //const int months = 12;
+            //const double pi = 3.14;
+            //const int weeks = 52;
+
+            //Console.WriteLine(months);
+            //Console.WriteLine(pi);
+            //Console.WriteLine(weeks);
+
+            Console.WriteLine("byet");
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("sbyet");
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("short");
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("ushort");
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("int");
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("uint");
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("longe");
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("ulong");
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("float");
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("double");
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("decimal");
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("char");
+            Console.WriteLine(char.MinValue);
+            Console.WriteLine(char.MaxValue);
+            Console.WriteLine("==============");
 
             
-            object R = 12;
+            
+            
 
-            bool f = false;
 
-
-            Console.WriteLine(num1);
-            Console.WriteLine(Name);
-            Console.WriteLine(Fadi);
-            Console.WriteLine(F);
-            Console.WriteLine(t);
-            Console.WriteLine(number);
-            Console.WriteLine(R);
-            Console.WriteLine(f);
             
             Console.Read();
         }
