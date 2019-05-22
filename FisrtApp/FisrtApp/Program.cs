13bae7c64c8e76edbc1dc4441893d21eaa6c9963
Fadi-Saibah <50773796+Fadi-Saibah@users.noreply.github.com>
2019-05-22 03:42:26 +0300

    Fadi

--- FisrtApp/FisrtApp/Program.cs
+++ FisrtApp/FisrtApp/Program.cs
@@ -75,72 +75,113 @@ namespace FisrtApp
             //Console.WriteLine(pi);
             //Console.WriteLine(weeks);
 
-            Console.WriteLine("byet");
-            Console.WriteLine(byte.MinValue);
-            Console.WriteLine(byte.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("byte");
+            //Console.WriteLine(byte.MinValue);
+            //Console.WriteLine(byte.MaxValue);
+            //Console.WriteLine(sizeof(byte));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("sbyet");
-            Console.WriteLine(sbyte.MinValue);
-            Console.WriteLine(sbyte.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("sbyet");
+            //Console.WriteLine(sbyte.MinValue);
+            //Console.WriteLine(sbyte.MaxValue);
+            //Console.WriteLine(sizeof(sbyte));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("short");
-            Console.WriteLine(short.MinValue);
-            Console.WriteLine(short.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("short");
+            //Console.WriteLine(short.MinValue);
+            //Console.WriteLine(short.MaxValue);
+            //Console.WriteLine(sizeof(short));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("ushort");
-            Console.WriteLine(ushort.MinValue);
-            Console.WriteLine(ushort.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("ushort");
+            //Console.WriteLine(ushort.MinValue);
+            //Console.WriteLine(ushort.MaxValue);
+            //Console.WriteLine(sizeof(ushort));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("int");
-            Console.WriteLine(int.MinValue);
-            Console.WriteLine(int.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("int");
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(sizeof(int));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("uint");
-            Console.WriteLine(uint.MinValue);
-            Console.WriteLine(uint.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("uint");
+            //Console.WriteLine(uint.MinValue);
+            //Console.WriteLine(uint.MaxValue);
+            //Console.WriteLine(sizeof(uint));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("longe");
-            Console.WriteLine(long.MinValue);
-            Console.WriteLine(long.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("long");
+            //Console.WriteLine(long.MinValue);
+            //Console.WriteLine(long.MaxValue);
+            //Console.WriteLine(sizeof(long));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("ulong");
-            Console.WriteLine(ulong.MinValue);
-            Console.WriteLine(ulong.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("ulong");
+            //Console.WriteLine(ulong.MinValue);
+            //Console.WriteLine(ulong.MaxValue);
+            //Console.WriteLine(sizeof(ulong));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("float");
-            Console.WriteLine(float.MinValue);
-            Console.WriteLine(float.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("float");
+            //Console.WriteLine(float.MinValue);
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine(sizeof(float));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("double");
-            Console.WriteLine(double.MinValue);
-            Console.WriteLine(double.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("double");
+            //Console.WriteLine(double.MinValue);
+            //Console.WriteLine(double.MaxValue);
+            //Console.WriteLine(sizeof(double));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("decimal");
-            Console.WriteLine(decimal.MinValue);
-            Console.WriteLine(decimal.MaxValue);
-            Console.WriteLine("==============");
+            //Console.WriteLine("decimal");
+            //Console.WriteLine(decimal.MinValue);
+            //Console.WriteLine(decimal.MaxValue);
+            //Console.WriteLine(sizeof(decimal));
+            //Console.WriteLine("==============");
 
-            Console.WriteLine("char");
-            Console.WriteLine(char.MinValue);
-            Console.WriteLine(char.MaxValue);
-            Console.WriteLine("==============");
-
-            
-            
-            
+            //Console.WriteLine("char");
+            //Console.WriteLine(char.MinValue);
+            //Console.WriteLine(char.MaxValue);
+            //Console.WriteLine(sizeof(char));
+            //Console.WriteLine("==============");
 
 
-            
+            char x1 = '\u0046';
+            char x2 = '\u0061';
+            char x3 = '\u0064';
+            char x4 = '\u0069';
+
+            Console.Write(x1);
+            Console.Write(x2);
+            Console.Write(x3);
+            Console.Write(x4);
+
+
+            string str1 = "Fadi";
+            string str2 = " ";
+            string str3 = "sy";
+
+            string strall = str1 + str2 + str3;
+
+            Console.WriteLine(strall);
+
+
+            string strName = "Fadi";
+            int num1 = 16;
+            char N = '\u0079';
+
+            string strali = strName + " " + num1 + " " + N;
+
+            Console.WriteLine(strali);
+
+
+
+
+
+
+
             Console.Read();
         }
     }
