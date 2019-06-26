2c1aed3f04f036b67b092a2abff2364e59c5408b
Fadi-Saibah <50773796+Fadi-Saibah@users.noreply.github.com>
2019-06-26 20:21:22 +0300

    Fadi

--- FisrtApp/FisrtApp/Program.cs
+++ FisrtApp/FisrtApp/Program.cs
@@ -1000,12 +1000,85 @@ namespace FisrtApp
 
             //==========================================================
 
+            //for (int dept = 1; dept <= 4; dept++)
+            //{
+            //    Console.WriteLine("Department" + dept);
 
+            //    for (int emp = 1; emp <=3; emp++)
+            //    {
+            //        Console.WriteLine("   employee" + emp);
 
+            //    }
+            //    Console.WriteLine("===================");
+            //}
 
+            //=============================================================
 
+            //for (int com = 1; com <=2; com++)
+            //{
+            //    Console.WriteLine("company " + com);
+            //    Console.WriteLine("______________________");
+            //    for (int b = 1; b <=2; b++)
+            //    {
+            //        Console.WriteLine("   building " + b);
+            //        Console.WriteLine("   ----------------");
+            //        for( int f=1; f <=3; f++)
+            //        {
+            //            Console.WriteLine("      floor" + f);
+            //            for (int r = 1; r <= 2; r++) 
+            //            {
+            //                Console.WriteLine("         room" + r);
+            //            }
+            //        }
 
+            //    }
+            //    Console.WriteLine("==================");
+            //}
 
+            //================================================================
+
+            //for (int baseNum = 1; baseNum <= 10; baseNum++) 
+            //{
+            //    Console.WriteLine("Table " + baseNum);
+            //    Console.WriteLine("--------------");
+            //    for (int num = 1; num <= 10; num++) 
+            //    {
+            //        Console.WriteLine("   " + baseNum + " x " + num + " = " + baseNum * num);
+
+            //    }
+            //}
+
+            //======================================================================================
+
+            //for (int baseNum = 1; baseNum <= 10; baseNum++)
+            //{
+            //    Console.WriteLine("Table " + baseNum);
+            //    Console.WriteLine("--------------");
+            //    for (int num = baseNum; num <= 10; num++)
+            //    {
+            //        Console.WriteLine("   " + baseNum + " x " + num + " = " + baseNum * num);
+
+            //    }
+            //    Console.WriteLine("=================");
+            //}
+
+            //=========================================================================================
+
+            //string strDT;
+            //strDT = DateTime.Now.ToString();
+
+            //Console.WriteLine(strDT);
+
+            //==========================================================
+
+            //Console.WriteLine(DateTime.Now);
+
+            //==========================================================
+
+            //DateTime dt;
+            //dt = Convert.ToDateTime("06/04/1922  13:00");
+
+            //Console.WriteLine(dt);
 
 
 
